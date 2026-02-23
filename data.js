8d821d89411359cc9e78f2bff7ed6f59f42e95e8
nadan0924 <nadan0924@gmail.com>
2026-02-23 15:52:10 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+window.CATALOG = {
+  title: "고객님 추천 매물 카탈로그",
+  subtitle: "요청 조건 기준으로 선별한 리스트입니다.",
+  items: [
+    {
+      name: "럭스하우스 매물 9272",
+      price: "가격/조건: 상세페이지 참고",
+      bullets: ["포인트 1", "포인트 2", "포인트 3"],
+      url: "https://luxehouse.kr/_lux/item.php?it_id=9272&list_link="
+    }
+  ]
+};
